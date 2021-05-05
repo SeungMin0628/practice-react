09f3882418ab250162827b1a593263f57c16c0b6
SeungMin0628 <smlee95kr@gmail.com>
2021-05-05 22:29:57 +0900

    sketch TVPresenter

--- src/routes/TV/TVPresenter.js
+++ src/routes/TV/TVPresenter.js
@@ -1,3 +1,40 @@
-const TVPresenter = () => 'TV'
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components';
+import Section from 'components/Section'
+import Loader from 'components/Loader'
+
+const Container = styled.div`
+  padding: 0 10px;
+`
+
+const TVPresenter = ({ topRated, airingToday, popular, error, loading }) =>
+  loading ? (<Loader />) : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Shows">
+          {topRated.map(show => <span id={show.id}>{show.name}</span>)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Shows">
+          {popular.map(show => <span id={show.id}>{show.name}</span>)}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing Today">
+          {airingToday.map(show => <span id={show.id}>{show.name}</span>)}
+        </Section>
+      )}
+    </Container>
+  )
+
+TVPresenter.propTypes = {
+  topRated: PropTypes.array,
+  airingToday: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+}
 
 export default TVPresenter
