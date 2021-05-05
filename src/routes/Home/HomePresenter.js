b55ba03838380c0c105e65dee4d4218bece1261e
SeungMin0628 <smlee95kr@gmail.com>
2021-05-05 22:29:31 +0900

    sketch HomePresenter

--- src/routes/Home/HomePresenter.js
+++ src/routes/Home/HomePresenter.js
@@ -1,3 +1,40 @@
-const HomePresenter = () => 'Movies'
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components';
+import Section from 'components/Section'
+import Loader from 'components/Loader'
+
+const Container = styled.div`
+  padding: 0 10px;
+`
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
+  loading ? (<Loader />) : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming">
+          {upcoming.map(movie => <span key={movie.id}>{movie.title}</span>)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular">
+          {popular.map(movie => <span key={movie.id}>{movie.title}</span>)}
+        </Section>
+      )}
+    </Container>
+  )
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upcoming: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+}
 
 export default HomePresenter
