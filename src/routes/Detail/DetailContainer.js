f2c4b841dca51938ac8092ff4cbdb1f89095422f
SeungMin0628 <smlee95kr@gmail.com>
2021-05-05 21:19:36 +0900

    define DetailContainer

--- src/routes/Detail/DetailContainer.js
+++ src/routes/Detail/DetailContainer.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DetailPresenter from './DetailPresenter'
+import { MoviesApi, TVApi } from 'api'
 
-const DetailContainer = () => {
-  const [result] = React.useState(null)
-  const [error] = React.useState(null)
-  const [loading] = React.useState(true)
+const DetailContainer = (props) => {
+  const [result, setResult] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+  // TODO: Delete this, we will accept isMovie from props
+  const [isMovie] = useState(props.location.pathname.includes('/movie/'))
+
+  useEffect(() => {
+    const getId = () => {
+      const { match: { params: { id } } } = props
+      return id
+    }
+
+    const validateId = id => {
+      const parsedId = Number(id)
+
+      if (isNaN(parsedId)) {
+        const { history: { push } } = props
+        return push('/')
+      }
+    }
+
+    const pullDatas = async id => {
+      try {
+        let result = null
+        if (isMovie) {
+          ({ data: result } = await MoviesApi.showDetail(id))
+        } else {
+          ({ data: result } = await TVApi.showDetail(id))
+        }
+        setResult(result)
+      } catch {
+        setError("Can't find result.")
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    const id = getId()
+    validateId(id)
+    pullDatas(id)
+  }, [props, isMovie])
 
   return(
     <DetailPresenter
